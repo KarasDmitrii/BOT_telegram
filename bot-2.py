85078fd32295385475b4584e78e05696a42bda06
KarasDmitrii <dimakaras83@gmail.com>
2022-10-06 18:53:51 +0400

    Изменён текст приветствия

--- bot-2.py
+++ bot-2.py
@@ -6,19 +6,23 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from urllib import parse
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 name = []
 link = []
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='helo tet13')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Для поиска введите название, или автора книги')
 
 
-def get_name_link(url_t):
-    try:
+def not_f(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Ничего не найдено')
+
+
+try:
+    def get_name_link(url_t):
         r = requests.get(url_t)
         soup = BeautifulSoup(r.text, 'lxml')
         for i in soup.find('ul', class_=False).find_all('li', class_=False):
@@ -26,42 +30,39 @@ def get_name_link(url_t):
             link.append('https://flibusta.site' + i.find('a').get('href') + '/fb2')
         n_book = int(soup.find('div', id='main').find('h3', style=True).text.replace('):', '').split(' ')[-1])
         return name, link, n_book
-    except:
-        not_f
 
 
-def not_f(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Ничего не найдено')
-
-
-def echo(update, context):
-    global name, link, n_book
-    name.clear()
-    link.clear()
-    mess = ''
-    args_dict = {'ask': update.message.text, 'chb': 'on'}
-    url_parts = list(parse.urlparse('https://flibusta.site'))
-    url_parts[2] = '/booksearch'
-    url_parts[4] = parse.urlencode(args_dict)
-    text1 = parse.urlunparse(url_parts)
-    name, link, n_book = get_name_link(text1)
-    if n_book > 5:
-        for i in range(5):
-            mess += f'{name[i]}\n{link[i]}\n\n'
-        keyboard = [
-            [InlineKeyboardButton('<==', callback_data='-'),
-             InlineKeyboardButton('==>', callback_data='+')]]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        update.message.reply_text(text=mess, reply_markup=reply_markup)
-    else:
-        for i in range(len(name)):
-            mess += f'{name[i]}\n{link[i]}\n\n'
-        context.bot.send_message(chat_id=update.effective_chat.id, text=mess)
+    def echo(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text='загрузка...')
+        global name, link, n_book
+        name.clear()
+        link.clear()
+        mess = ''
+        args_dict = {'ask': update.message.text, 'chb': 'on'}
+        url_parts = list(parse.urlparse('https://flibusta.site'))
+        url_parts[2] = '/booksearch'
+        url_parts[4] = parse.urlencode(args_dict)
+        text1 = parse.urlunparse(url_parts)
+        name, link, n_book = get_name_link(text1)
+        if n_book > 5:
+            for i in range(5):
+                mess += f'{name[i]}\n{link[i]}\n\n'
+            keyboard = [
+                [InlineKeyboardButton('<==', callback_data='-'),
+                 InlineKeyboardButton('==>', callback_data='+')]]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            update.message.reply_text(text=mess, reply_markup=reply_markup)
+        else:
+            for i in range(len(name)):
+                mess += f'{name[i]}\n{link[i]}\n\n'
+            context.bot.send_message(chat_id=update.effective_chat.id, text=mess)
+except BaseException:
+    not_f
 
 
 g = 0
 def button(update, _):
-    global name, link, g
+    global g
     query = update.callback_query
     variant = query.data
     query.answer()
@@ -85,7 +86,6 @@ def button(update, _):
 
 def main():
     TOKEN = '5513616314:AAFO0qwEuZRL557radde4Z5fFGAsUrTnKWs'
-    # updater = Updater(token=TOKEN, use_context=True)
     updater = Updater(token=TOKEN)
     dispatcher = updater.dispatcher
     start_handler = CommandHandler('start', start)
